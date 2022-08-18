5fd3a1dec4c9ca0cf27af5451d991485f7823f4e
saltfish <1218838196@qq.com>
2022-08-18 14:02:37 +0800

    bugfix

--- config/run-zlm.sh
+++ config/run-zlm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp /config/config.ini /opt/media/config.ini
 cd /opt/assist
-nohup java -jar *.jar --spring.config.location=/opt/assist/config/application.assist.yml &
+nohup java -jar *.jar --spring.config.location=/config/application.assist.yml &
 /opt/media/MediaServer -d -m 3
