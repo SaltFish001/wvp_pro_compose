ba350ccec966dafad83cd37e5a9f09dfcb05c843
saltfish <1218838196@qq.com>
2022-08-18 14:54:14 +0800

    bugfix

--- wvp/config/run.sh
+++ wvp/config/run.sh
@@ -2,6 +2,7 @@
 # echo ${WVP_IP}
 # echo ${WVP_CONFIG}
 # redis-server --daemonize yes --bind 0.0.0.0
+cp /config/config.ini /opt/media/config.ini
 cd /opt/assist
 nohup java -jar *.jar --spring.config.location=/opt/assist/config/application.yml &
 nohup /opt/media/MediaServer -d -m 3 &
